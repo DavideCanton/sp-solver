1299befc81bcdf09e9c3d90c44bc5300fca60a56
DavideCanton <davide.canton5@gmail.com>
2015-05-24 18:48:53 +0200

    Fixed includes

--- SpSolver/LevelDetect.cpp
+++ SpSolver/LevelDetect.cpp
@@ -1,5 +1,5 @@
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <set>
 #include "Coords.h"
 #include "LevelDetect.h"
